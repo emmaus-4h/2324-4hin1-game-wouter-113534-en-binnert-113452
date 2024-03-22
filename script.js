ad59f29b170166766e71834242d11e086c4bd3df
Binnert-H <146429779+Binnert-H@users.noreply.github.com>
2024-03-22 11:28:57 +0000

    vijand en speler kunnen bewegen en game over en uitleg scherm en nog wat meer.

--- script.js
+++ script.js
@@ -17,11 +17,16 @@
 /* ********************************************* */
 const SPELEN = 1;
 const GAMEOVER = 2;
+const UITLEG  = 8;
 var spelStatus = SPELEN;
 
 var spelerX = 600; // x-positie van speler
 var spelerY = 600; // y-positie van speler
 var health = 100;  // health van speler
+
+var vijandX = 600; // x-positie van de vijand
+var vijandY = 600; // y-positie van de vijand
+
 
 /* ********************************************* */
 /* functies die je gebruikt in je game           */
@@ -33,14 +38,31 @@ var health = 100;  // health van speler
 var beweegAlles = function() {
   // speler
     if (keyIsDown(65)) {
-      spelerX = spelerX -1;
+      spelerX = spelerX -2;
     }
     if (keyIsDown(68)) {
-      spelerX = spelerX +1;
+      spelerX = spelerX +2;
     }
-    
+     if (keyIsDown(87)){
+       spelerY = spelerY -2;
+     }
+     if (keyIsDown(83)){
+       spelerY = spelerY +2;
+     }
+  
   // vijand
-
+   if (keyIsDown(LEFT_ARROW)) {
+     vijandX = vijandX -2;
+   }
+   if (keyIsDown(RIGHT_ARROW)) {
+     vijandX = vijandX +2;
+   }
+    if (keyIsDown(UP_ARROW)){
+      vijandY = vijandY -2;
+    }
+    if (keyIsDown(DOWN_ARROW)){
+      vijandY = vijandY +2;
+    }
   // kogel
 };
 
@@ -51,7 +73,12 @@ var beweegAlles = function() {
  */
 var verwerkBotsing = function() {
   // botsing speler tegen vijand
-
+    if (spelerX - vijandX < 50 &&
+        spelerX - vijandX > -50 &&
+        spelerY - vijandY < 50 &&
+        spelerY - vijandY > -50) {
+    console.log("botsing");
+    }
   // botsing kogel tegen vijand
 
   // update punten en health
@@ -64,9 +91,12 @@ var verwerkBotsing = function() {
 var tekenAlles = function() {
   // achtergrond
   fill("green");
-  rect(0,0,1280,720);
+  rect(0,0, width, height);
   // vijand
-
+  fill("red");
+  rect(vijandX - 25 ,vijandY - 25 , 50, 50);
+  fill("black");
+  ellipse(vijandX, vijandY , 10, 10);
   // kogel
 
   // speler
@@ -109,8 +139,31 @@ function draw() {
     if (health <= 0) {
       spelStatus = GAMEOVER;
     }
+       console.log("spelen");
   }
   if (spelStatus === GAMEOVER) {
     // teken game-over scherm
+    console.log("game over");
+    textSize(60);
+    fill("white");
+    text("game over, druk spatie voor start", 600, 400);
+    if (keyIsDown(32)) { //spatie
+    spelerX = 400;
+    spelStatus = SPELEN;
+    }
   }
+  
+   if (spelStatus === UITLEG) {
+     // teken uitleg scherm
+     console.log("uitleg");
+     textSize(60);
+     fill("green");
+     rect(0, 0, 1280, 720);
+     fill("white");
+     text("uitleg: doe je ding en druk op enter", 600 ,400);
+     if (keyIsDown(13)) { //enter}
+       spelerX = 400;
+       spelStatus = SPELEN;
+  }
+
 }
