efb33896aa6469e6885c92318bbe95b45ed2c428
Binnert-H <146429779+Binnert-H@users.noreply.github.com>
2024-04-12 08:59:19 +0000

    snelheid, game doet het na twee weken weer

--- script.js
+++ script.js
@@ -15,18 +15,20 @@
 /* ********************************************* */
 /* globale variabelen die je gebruikt in je game */
 /* ********************************************* */
+var aantal = 0;
+
 const SPELEN = 1;
 const GAMEOVER = 2;
-const UITLEG  = 8;
-var spelStatus = SPELEN;
+const UITLEG = 8;
+var spelStatus = UITLEG;
 
 var spelerX = 600; // x-positie van speler
 var spelerY = 600; // y-positie van speler
-var health = 100;  // health van speler
+
 
 var vijandX = 600; // x-positie van de vijand
 var vijandY = 600; // y-positie van de vijand
-
+var snelheid = 2;
 
 /* ********************************************* */
 /* functies die je gebruikt in je game           */
@@ -37,32 +39,32 @@ var vijandY = 600; // y-positie van de vijand
  */
 var beweegAlles = function() {
   // speler
-    if (keyIsDown(65)) {
-      spelerX = spelerX -2;
-    }
-    if (keyIsDown(68)) {
-      spelerX = spelerX +2;
-    }
-     if (keyIsDown(87)){
-       spelerY = spelerY -2;
-     }
-     if (keyIsDown(83)){
-       spelerY = spelerY +2;
-     }
-  
+  if (keyIsDown(65)) {
+    spelerX = spelerX - snelheid;
+  }
+  if (keyIsDown(68)) {
+    spelerX = spelerX + snelheid;
+  }
+  if (keyIsDown(87)) {
+    spelerY = spelerY - snelheid;
+  }
+  if (keyIsDown(83)) {
+    spelerY = spelerY + snelheid;
+  }
+
   // vijand
-   if (keyIsDown(LEFT_ARROW)) {
-     vijandX = vijandX -2;
-   }
-   if (keyIsDown(RIGHT_ARROW)) {
-     vijandX = vijandX +2;
-   }
-    if (keyIsDown(UP_ARROW)){
-      vijandY = vijandY -2;
-    }
-    if (keyIsDown(DOWN_ARROW)){
-      vijandY = vijandY +2;
-    }
+  if (keyIsDown(LEFT_ARROW)) {
+    vijandX = vijandX - snelheid;
+  }
+  if (keyIsDown(RIGHT_ARROW)) {
+    vijandX = vijandX + snelheid;
+  }
+  if (keyIsDown(UP_ARROW)) {
+    vijandY = vijandY - snelheid;
+  }
+  if (keyIsDown(DOWN_ARROW)) {
+    vijandY = vijandY + snelheid;
+  }
   // kogel
 };
 
@@ -73,30 +75,24 @@ var beweegAlles = function() {
  */
 var verwerkBotsing = function() {
   // botsing speler tegen vijand
-    if (spelerX - vijandX < 50 &&
-        spelerX - vijandX > -50 &&
-        spelerY - vijandY < 50 &&
-        spelerY - vijandY > -50) {
-    console.log("botsing");
-    }
+
   // botsing kogel tegen vijand
 
   // update punten en health
-
 };
 
-/**
- * Tekent spelscherm
- */
+
+
 var tekenAlles = function() {
+
   // achtergrond
   fill("green");
-  rect(0,0, width, height);
+  rect(0, 0, width, height);
   // vijand
   fill("red");
-  rect(vijandX - 25 ,vijandY - 25 , 50, 50);
+  rect(vijandX - 25, vijandY - 25, 50, 50);
   fill("black");
-  ellipse(vijandX, vijandY , 10, 10);
+  ellipse(vijandX, vijandY, 10, 10);
   // kogel
 
   // speler
@@ -109,9 +105,18 @@ var tekenAlles = function() {
 
 };
 
-/* ********************************************* */
-/* setup() en draw() functies / hoofdprogramma   */
-/* ********************************************* */
+var checkGameOver = function() {
+  if (spelerX - vijandX < 50 &&
+    spelerX - vijandX > -50 &&
+    spelerY - vijandY < 50 &&
+    spelerY - vijandY > -50) {
+    aantal = aantal + 1
+    console.log("botsing" + aantal);
+    return true;
+  }
+
+  return false;
+}
 
 /**
  * setup
@@ -136,34 +141,32 @@ function draw() {
     beweegAlles();
     verwerkBotsing();
     tekenAlles();
-    if (health <= 0) {
+    if (checkGameOver()) {
       spelStatus = GAMEOVER;
     }
-       console.log("spelen");
   }
   if (spelStatus === GAMEOVER) {
     // teken game-over scherm
     console.log("game over");
     textSize(60);
     fill("white");
-    text("game over, druk spatie voor start", 600, 400);
+    text("game over, druk spatie voor start", 200, 400);
     if (keyIsDown(32)) { //spatie
-    spelerX = 400;
-    spelStatus = SPELEN;
+      spelStatus = UITLEG;
     }
   }
-  
-   if (spelStatus === UITLEG) {
-     // teken uitleg scherm
-     console.log("uitleg");
-     textSize(60);
-     fill("green");
-     rect(0, 0, 1280, 720);
-     fill("white");
-     text("uitleg: doe je ding en druk op enter", 600 ,400);
-     if (keyIsDown(13)) { //enter}
-       spelerX = 400;
-       spelStatus = SPELEN;
-  }
 
+  if (spelStatus === UITLEG) {
+    // teken uitleg scherm
+    console.log("uitleg");
+    textSize(60);
+    fill("green");
+    rect(0, 0, 1280, 720);
+    fill("white");
+    text("uitleg: doe je ding en druk op enter", 200 ,300);
+    if (keyIsDown(13)) { //enter
+      spelerX = 400;
+      spelStatus = SPELEN;
+    };
+  }
 }
